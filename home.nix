34bbe30f2d953086778ea310b1e01c7ac0b4e5b2
minjunj <minjun_jo@gm.gist.ac.kr>
2025-04-13 17:13:26 +0900

    1

--- home.nix
+++ home.nix
@@ -7,36 +7,43 @@
 
   # 설치할 패키지들
   home.packages = with pkgs; [
-    # 기본 도구들
     git
     vim
     curl
     wget
     htop
-    
-    # 필요한 패키지를 더 추가하세요
-    # firefox
-    # vscode
-    # 기타 등등
   ];
 
-  # 프로그램별 설정
-  programs = {
-    bash = {
+  # zsh 설정
+  programs.zsh = {
+    enable = true;
+    enableAutosuggestions = true;
+    enableCompletion = true;
+    syntaxHighlighting.enable = true;
+    
+    # oh-my-zsh 설정 (선택사항)
+    oh-my-zsh = {
       enable = true;
-      shellAliases = {
-        ll = "ls -l";
-        update = "sudo apt update && sudo apt upgrade";
-      };
+      plugins = [ "git" "sudo" "docker" ];
+      theme = "robbyrussell";  # 또는 다른 테마
     };
     
-    git = {
-      enable = true;
-      userName = "minjunj";
-      userEmail = "minjun_jo@gm.gist.ac.kr";
-    };
-    
-    # 필요한 프로그램 설정을 더 추가하세요
+    # 추가 zsh 설정
+    initExtra = ''
+      # 여기에 추가 zsh 설정 입력
+      alias ll='ls -l'
+      alias update='sudo apt update && sudo apt upgrade'
+    '';
+  };
+  
+  # 기본 셸로 zsh 설정
+  programs.bash.enable = false;  # bash 설정 비활성화
+  
+  # Git 설정은 그대로 유지
+  programs.git = {
+    enable = true;
+    userName = "minjunj";
+    userEmail = "minjun_jo@gm.gist.ac.kr";
   };
 
   # Home Manager가 자신을 관리하도록 허용
