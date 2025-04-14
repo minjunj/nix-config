ec8d560715fad6694fc76485174a68fb8ffb62b4
minjunj <minjun_jo@gm.gist.ac.kr>
2025-04-14 22:28:10 +0900

    Nix OS

--- hosts/iso-test.nix
+++ hosts/iso-test.nix
@@ -12,9 +12,24 @@
     isNormalUser = true;
     extraGroups = [ "wheel" ];
     password = "1234";
+    shell = pkgs.zsh;  # 기본 쉘을 zsh로 설정
   };
 
   services.sshd.enable = true;
+
+  # zsh를 설치
+  environment.systemPackages = with pkgs; [
+    zsh
+  ];
+
+  # zsh 설정 활성화
+  programs.zsh.enable = true;
+
+  # 시스템 설정이므로 필요한 zsh 설정을 추가할 수 있음
+  users.users.nixos.zshrc = ''
+    export ZSH=$HOME/.oh-my-zsh
+    export ZSH_THEME="agnoster"  # 원하는 테마로 설정 가능
+  '';
 
   system.stateVersion = "24.11";
 }
