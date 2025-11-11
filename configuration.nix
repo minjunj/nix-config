f892efa171b78358ff5a6735e67260276fab6ae7
minjunj <minjun_jo@gm.gist.ac.kr>
2025-11-11 23:45:40 +0900

    화면이 나옴2

--- configuration.nix
+++ configuration.nix
@@ -5,11 +5,28 @@
 
   services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true;
+  programs.sway = {
+    enable = true;
+    wrapperFeatures.gtk = true;
+  };
+
+  users.users.alice = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "video" "input" "seat" ];
+    initialPassword = "test";
+  };
 
   environment.systemPackages = with pkgs; [
     cowsay
     lolcat
   ];
+
+  # 자동으로 Sway 실행
+  environment.loginShellInit = ''
+    if [ -z "$WAYLAND_DISPLAY" ] && [ "$XDG_VTNR" -eq 1 ]; then
+      exec sway
+    fi
+  '';
 
   system.stateVersion = "24.05";
 }
