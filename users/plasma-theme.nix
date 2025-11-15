7a991b0d8ba77c342e39669c70755af062de038d
minjunj <minjun_jo@gm.gist.ac.kr>
2025-11-15 19:55:31 +0900

    v4.2 : plasma thema

--- users/plasma-theme.nix
+++ users/plasma-theme.nix
@@ -1,36 +1,18 @@
 {pkgs, ...}: let
-  apple-ventura-dark = pkgs.stdenv.mkDerivation {
-    pname = "apple-ventura-dark";
-    version = "1.0.0";
-
-    src = pkgs.fetchFromGitHub {
-      owner = "zayronxio";
-      repo = "Apple-theme-Plasma";
-      rev = "93ed9d35c13d1711e887e6cb4f222063496c28ab";
-      hash = "";  # First run will show the correct hash
-    };
-
-    installPhase = ''
-      runHook preInstall
-
-      # Install plasma desktop themes
-      mkdir -p $out/share/plasma/desktoptheme
-      for theme in Apple-BigSur-LLT AppleDark-ALL AppleDark-ALL-AccentDinamic Apple-Ventura; do
-        cp -r "$theme" $out/share/plasma/desktoptheme/
-      done
-
-      # Install look-and-feel
-      mkdir -p $out/share/plasma/look-and-feel
-      cp -r look-and-feel/* $out/share/plasma/look-and-feel/
-
-      runHook postInstall
-    '';
-
-    meta = {
-      description = "Apple Ventura Dark look and feel for KDE Plasma";
-      homepage = "https://github.com/zayronxio/Apple-theme-Plasma";
-    };
+  apple-theme-src = pkgs.fetchFromGitHub {
+    owner = "zayronxio";
+    repo = "Apple-theme-Plasma";
+    rev = "93ed9d35c13d1711e887e6cb4f222063496c28ab";
+    hash = "";  # First run will show the correct hash
   };
 in {
-  home.packages = [apple-ventura-dark];
+  # Install plasma desktop themes directly to ~/.local/share
+  home.file = {
+    ".local/share/plasma/desktoptheme/Apple-BigSur-LLT".source = "${apple-theme-src}/Apple-BigSur-LLT";
+    ".local/share/plasma/desktoptheme/AppleDark-ALL".source = "${apple-theme-src}/AppleDark-ALL";
+    ".local/share/plasma/desktoptheme/AppleDark-ALL-AccentDinamic".source = "${apple-theme-src}/AppleDark-ALL-AccentDinamic";
+    ".local/share/plasma/desktoptheme/Apple-Ventura".source = "${apple-theme-src}/Apple-Ventura";
+
+    ".local/share/plasma/look-and-feel/Apple-Ventura-Dark".source = "${apple-theme-src}/look-and-feel/Apple-Ventura-Dark";
+  };
 }
