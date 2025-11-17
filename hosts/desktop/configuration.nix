d48ddbb2941146edab82f4cbb6d675cda9c274c4
minjunj <minjun_jo@gm.gist.ac.kr>
2025-11-17 20:20:14 +0900

    v6.0 : add desktop

---
+++ hosts/desktop/configuration.nix
@@ -0,0 +1,37 @@
+# nuc_n100 host-specific configuration
+{
+  inputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  imports = [
+    ../../nixos/common.nix
+    ./hardware-configuration.nix
+    inputs.home-manager.nixosModules.home-manager
+    ../../users/minjunj.nix
+  ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Set hostname for this host
+  networking.hostName = "desktop";
+  networking.networkmanager.enable = true;
+
+  # Enable KDE Plasma on Wayland
+  services.desktopManager.plasma6.enable = true;
+  services.displayManager.sddm.enable = true;
+  services.displayManager.sddm.wayland.enable = true;
+
+  # home-manager configuration
+  home-manager = {
+    useGlobalPkgs = true;
+    useUserPackages = true;
+    extraSpecialArgs = {inherit inputs;};
+    sharedModules = [
+      inputs.plasma-manager.homeModules.plasma-manager
+    ];
+  };
+}
