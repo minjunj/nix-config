c2df7a0c3552421452ddb2f3a2cc063d6bfb0553
minjunj <minjun_jo@gm.gist.ac.kr>
2025-11-24 15:13:07 +0900

    v7.1.1 : fcitx5 삭제. kime 추가

--- kvm/fcitx5.nix
+++ kvm/fcitx5.nix
@@ -1,18 +1,12 @@
 { pkgs, ... }:
 {
-  # Korean input method (fcitx5)
-  i18n.inputMethod = {
-    enable = true;
-    type = "fcitx5";
-    fcitx5 = {
-      waylandFrontend = true;
-      addons = with pkgs; [
-        kdePackages.fcitx5-qt
-        fcitx5-gtk
-        fcitx5-hangul
-        fcitx5-mozc
-        fcitx5-fluent
-      ];
+  # Korean input method
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    inputMethod = {
+      enable = true;
+      type = "kime";
+      kime.iconColor = "White";
     };
-  };
+  }
 }
