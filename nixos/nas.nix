779f3db706d05daf595d1714329c6b6483b5dba7
minjunj <minjun_jo@gm.gist.ac.kr>
2026-02-06 16:59:12 +0900

    v13.2 : update temp

--- nixos/nas.nix
+++ nixos/nas.nix
@@ -5,22 +5,15 @@
   pkgs,
   ...
 }: {
-  # Install CIFS utilities
-  environment.systemPackages = with pkgs; [
-    cifs-utils
-  ];
-
-  fileSystems."/nas/share" = {
+  # For mount.cifs, required unless domain name resolution is not needed.
+  environment.systemPackages = [ pkgs.cifs-utils ];
+  fileSystems."/mnt/share" = {
     device = "//192.168.0.18/share";
     fsType = "cifs";
-    options = [
-      "credentials=/home/minjunj/nix-config/secret/smb-secret"
-      "uid=1000"
-      "gid=100"
-      "vers=3.0"
-      "noperm"
-      "x-systemd.device-timeout=5s"
-      "x-systemd.mount-timeout=5s"
-    ];
+    options = let
+      # this line prevents hanging on network split
+      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+
+    in ["${automount_opts},credentials=/home/minjunj/nix-config/secret/smb-secret"];
   };
 }
