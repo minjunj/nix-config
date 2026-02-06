610c3246408ecf30e3980bf9721f5389da11b1ed
minjunj <minjun_jo@gm.gist.ac.kr>
2026-02-06 15:55:34 +0900

    v13.2 : update temp

--- nixos/nas.nix
+++ nixos/nas.nix
@@ -13,10 +13,12 @@
   fileSystems."/nas/share" = {
     device = "//192.168.0.18/share";
     fsType = "cifs";
-    options = let
-      # this line prevents hanging on network split
-      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-
-    in ["${automount_opts},credentials=/home/minjunj/nix-config/secret/smb-secret,uid=1000,gid=100"];
+    options = [
+      "credentials=/home/minjunj/nix-config/secret/smb-secret"
+      "uid=1000"
+      "gid=100"
+      "x-systemd.device-timeout=5s"
+      "x-systemd.mount-timeout=5s"
+    ];
   };
 }
