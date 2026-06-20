285a69e2f6777d67f4a850cc3f88b060624482a6
minjunj <minjun_jo@gm.gist.ac.kr>
2026-06-20 16:31:46 +0900

    v25 : to set swaylock

--- modules/desktop/window_manager/tiling/noctalia.nix
+++ modules/desktop/window_manager/tiling/noctalia.nix
@@ -6,6 +6,46 @@
       inputs.noctalia.homeModules.default
     ];
 
+    programs.swaylock = {
+      enable = true;
+      package = pkgs.swaylock-effects;
+      settings = {
+        image = "${../../../../assets/gup2.jpg}";
+        effect-blur = "7x5";
+        scaling = "fill";
+        color = "1e1e2e";
+
+        indicator = true;
+        clock = true;
+        show-failed-attempts = true;
+
+        font = "Noto Sans CJK KR";
+        font-size = 24;
+
+        indicator-radius = 72;
+        indicator-thickness = 4;
+
+        inside-color = "00000044";
+        ring-color = "ffffff66";
+        line-color = "00000000";
+        text-color = "f5f5f5";
+        key-hl-color = "f9e2af";
+        separator-color = "00000000";
+
+        inside-clear-color = "fab38733";
+        ring-clear-color = "fab387aa";
+        text-clear-color = "f5f5f5";
+
+        inside-ver-color = "a6e3a133";
+        ring-ver-color = "a6e3a1aa";
+        text-ver-color = "f5f5f5";
+
+        inside-wrong-color = "f38ba833";
+        ring-wrong-color = "f38ba8aa";
+        text-wrong-color = "f5f5f5";
+      };
+    };
+
     # configure options
     programs.noctalia-shell = {
       enable = true;
